1585885c492fb540da9f6d9b166aef81d4f5905b
baziz_meriem <mrmbaziz@gmail.com>
2022-12-19 23:42:28 +0100

    ADD Création d'un utilisateur

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -1,10 +1,42 @@
 package com.telly.controllers;
 
+import com.telly.dao.FormValidationGroup;
+import com.telly.dao.User;
+import com.telly.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+
+import java.security.Principal;
+
 public class UserController {
+    @Autowired
+    UserService userService;
     public String showLogin() {
         return "login";
     }
     public String showLogout() {
         return "loggedout";
+    }
+    public String createAccount(Model model, Principal principal) {
+
+        model.addAttribute("user", new User());
+
+        return "createaccount";
+    }
+    public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "createaccount";
+        }
+
+        user.setAuthority("ROLE_USER");
+        user.setEnabled(true);
+
+        userService.create(user);
+
+        return "home";
+
     }
 }
